11d03eabe840dba849753706c9f1c065f2adbe25
sampathsrikanth1999 <82893335+sampathsrikanth1999@users.noreply.github.com>
2026-07-08 04:03:07 +0000

    Updated the file with actual code

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,11 @@
 // Complete Guide to C++ Programming Foundations
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+#include <iostream>
+
+int main()
+{
+    std::cout<<"Hello there!!";
+    std::cout<<std::endl;
+}
+
